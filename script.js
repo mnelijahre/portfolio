684b376d1c89b8c83f56585aa5bb6ea702913ad7
Elijah Erickson <mnelijahre@gmail.com>
2024-08-23 14:54:35 -0500

    website page added

---
+++ script.js
@@ -0,0 +1,20 @@
+const projectPage = document.querySelector("#projectPage");
+const pageList = document.querySelector("#pageList");
+
+const doge = document.querySelector("#doge");
+const exit = document.querySelector("#x");
+
+
+function toggleDisplay(){
+    projectPage.classList.toggle("hidden");
+    pageList.classList.toggle("hidden");
+
+}
+
+exit.addEventListener("click", ()=>{
+    toggleDisplay();
+})
+
+doge.addEventListener("click", ()=>{
+    toggleDisplay();
+})
